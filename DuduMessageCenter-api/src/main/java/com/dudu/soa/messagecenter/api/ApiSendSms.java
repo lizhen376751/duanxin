622b38f32c89b1f124e7a318d0aea70c9aa1fc6a
lizhen <724577807@qq.com>
2017-02-22 11:30:34 +0800

    1

--- DuduMessageCenter-api/src/main/java/com/dudu/soa/messagecenter/api/ApiSendSms.java
+++ DuduMessageCenter-api/src/main/java/com/dudu/soa/messagecenter/api/ApiSendSms.java
@@ -4,9 +4,13 @@ import com.dudu.soa.messagecenter.module.ParameterEntry;
 
 public interface ApiSendSms {
 	
-	/*
-	 * 短信发送
-	 *  ParameterEntry实体类,没有配置默认为空
-	 */
+	 /**
+     * 
+    * @Title:       发送短信消息
+    * @Description: 
+    * @param ParameterEntry (构建实体类,只需要传入发送短信的参数即可,不传默认为空)
+    * @param:          
+    * @Date     
+     */
 	public void sendSMS(ParameterEntry parameterEntry);
 }
