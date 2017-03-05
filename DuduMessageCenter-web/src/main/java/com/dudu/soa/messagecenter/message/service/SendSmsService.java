b6147aa9140718ab9c254241b80b1863fedf45f2
lizhen <724577807@qq.com>
2017-03-05 12:00:53 +0800

    新加方法

--- DuduMessageCenter-web/src/main/java/com/dudu/soa/messagecenter/message/service/SendSmsService.java
+++ DuduMessageCenter-web/src/main/java/com/dudu/soa/messagecenter/message/service/SendSmsService.java
@@ -58,7 +58,7 @@ public class SendSmsService implements ApiSendSms{
 	
 	@Override
 	public void sendSMS(String shopcode, String businessType,
-			List<String> recnum,ParameterEntry parameterEntry) {
+		List<String> recnum,ParameterEntry parameterEntry) {
 		String feedback = "";
 		String carnum = parameterEntry.getCarnum();
 		String storeName = parameterEntry.getStoreName();
@@ -207,8 +207,155 @@ private String convertStreamToString(InputStream is) {
     return sb1.toString();    
 }
 	
-
+//测试方法
+public String sendSMS2(String shopcode, String businessType,
+	List<String> recnum,ParameterEntry parameterEntry) {
+	String feedback = "";
+	String carnum = parameterEntry.getCarnum();
+	String storeName = parameterEntry.getStoreName();
+	String date = parameterEntry.getDate();
+	String parameter1 = parameterEntry.getParameter1();
+	String parameter2 = parameterEntry.getParameter2();
+	String parameter3 = parameterEntry.getParameter3();
+	String parameter4 = parameterEntry.getParameter4();
 	
+	String sms = parameterEntry.getSms();
+	String smsPwd = parameterEntry.getSmsPwd();
+	String smsUser = parameterEntry.getSmsUser();
+	
+	
+	//获取AccessKey实体
+	String appkey = "";
+	String appSecret = "";
+	String signName = "";
+	String keyuse = "";
+	AccessKey accessKey = messageConfigDao.getAccessKey(shopcode);
+	if(accessKey!=null && !"".equals(accessKey)){
+		 appkey = accessKey.getAppkey();
+		 appSecret = accessKey.getAppSecret();
+		 //获取短信签名
+		 signName = accessKey.getSignName();
+		 //获取是否启用
+		 keyuse = accessKey.getkeyuse();
+	}
+	
+	//获取短信模板TemplateCode实体
+	String templateCode = "";
+	String templateUse = "";
+	TemplateCode templateCodes = messageConfigDao.getTemplateCode(shopcode, businessType);
+	if(templateCodes != null && !"".equals(templateCodes)){
+		//短信模板code
+		 templateCode = templateCodes.getTemplateCode();
+		 //获取是否启用
+		 templateUse = templateCodes.getTemplateUse();
+	}
+	
+	String sendPhone = "";
+	if(recnum.size()>0){
+		for(int i=0;i<recnum.size();i++){
+			sendPhone+=recnum.get(i);
+			sendPhone+=",";
+				
+		}
+	}
+	if(recnum.size()==1){
+		sendPhone = recnum.get(0);
+	}
+	
+	 
+//        原来的短信模式不需要添加
+//        node.put("sms", sms);
+//        node.put("smsPwd", smsPwd);
+//        node.put("smsUser", smsUser);
+       
+	try 
+	{	//现在的短信模式
+		if("true".equals(keyuse) && "true".equals(templateUse)){
+				JSONObject node = new JSONObject();
+		        node.put("carnum", carnum);
+		        node.put("date", date);
+		        node.put("storeName", storeName);
+		        node.put("parameter1", parameter1);
+		        node.put("parameter2", parameter2);
+		        node.put("parameter3",parameter3);
+		        node.put("parameter4", parameter4);
+				IClientProfile profile = DefaultProfile.getProfile(REGIONID, appkey, appSecret);
+				DefaultProfile.addEndpoint(REGIONID, REGIONID, "Sms",  HOST);
+				IAcsClient client = new DefaultAcsClient(profile);
+			    SingleSendSmsRequest request = new SingleSendSmsRequest();
+		        request.setSignName(signName);//控制台创建的签名名称
+		        request.setTemplateCode(templateCode);//控制台创建的模板CODE
+		        request.setParamString(node.toString());//短信模板中的变量；数字需要转换为字符串；个人用户每个变量长度必须小于15个字符。"
+		        request.setRecNum(sendPhone);//接收号码
+		        SingleSendSmsResponse httpResponse = client.getAcsResponse(request);
+		        return "短信发送成功!";
+		}else{
+			if( null != smsUser && smsUser.length() > 0 && !smsUser.isEmpty() &&
+					null != recnum && recnum.size() > 0 && !recnum.isEmpty() &&
+					null != smsPwd && smsPwd.length() > 0 && ! smsPwd.isEmpty() &&
+							null != sms && sms.length() > 0 && !sms.isEmpty())
+			{
+				//原来的短信发送模式
+				StringBuilder sb = new StringBuilder(AGOHOST);
+				sb.append("name=" + smsUser);
+				sb.append("&pwd=" + smsPwd);
+				sb.append("&mobile=" + sendPhone);
+				sb.append("&content=" + URLEncoder.encode(sms, "UTF-8"));
+				sb.append("&stime=");
+				sb.append("&sign=");
+				sb.append("&type=pt&extno=");
+				URL url = new URL(sb.toString());
+				HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+				connection.setRequestMethod("POST");
+				InputStream is = url.openStream();
+				String returnStr = convertStreamToString(is);//返回值
+				return "短信发送成功!";
+			}
+		}	
+	} 
+	catch (Exception e) 
+	{
+		e.printStackTrace();
+		feedback = feedback(e.toString());
+		return feedback;
+    }
+	return "短信未发送!";
 }
 
+	//错误提示
+private String feedback(String errorMess) {  
+	errorMess=errorMess.toString();
+	String error1= "The specified dayu status is wrongly formed";
+	String mess1 = "账户短信开通状态不正确!";
+	String error2="The specified sign name is wrongly formed";
+	String mess2 = "短信签名不正确或签名状态不正确!";
+	String error3="The specified templateCode is wrongly formed";
+	String mess3 = "短信模板Code不正确或者模板状态不正确!";
+	String error4="The specified recNum is wrongly formed";
+	String mess4 = "目标手机号不正确，单次发送数量不能超过100!";
+	String error5="The specified paramString and template is wrongly formed";
+	String mess5 = "短信模板中变量与模板内容不匹配!";
+	String error6="The specified paramString and template is wrongly formed";
+	String mess6 = "参数变量过长,已超过15个字符!";
+	if(errorMess.contains(error1)){
+		errorMess = mess1;
+	}
+	if(errorMess.contains(error2)){
+		errorMess = mess2;
+	}
+	if(errorMess.contains(error3)){
+		errorMess = mess3;
+	}
+	if(errorMess.contains(error4)){
+		errorMess = mess4;
+	}
+	if(errorMess.contains(error5)){
+		errorMess = mess5;
+	}
+	if(errorMess.contains(error6)){
+		errorMess = mess6;
+	}
+   return errorMess;
+}
 
+}
--- DuduMessageCenter-web/src/test/java/com/dudu/soa/messagecenter/message/SendSmsTest.java
+++ DuduMessageCenter-web/src/test/java/com/dudu/soa/messagecenter/message/SendSmsTest.java
@@ -11,9 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.dudu.soa.messagecenter.TestBase;
 import com.dudu.soa.messagecenter.message.module.ParameterEntry;
 import com.dudu.soa.messagecenter.message.service.SendSmsService;
-
-
-
 public class SendSmsTest extends TestBase{
 	
 	private static Logger logger = LoggerFactory.getLogger(SendSmsTest.class); 
@@ -38,6 +35,31 @@ public class SendSmsTest extends TestBase{
 			System.out.println("+++=======短信实体类"+parameterEntry);
 		    sendSmsService.sendSMS("0533001", "测试", list, parameterEntry);
 	}catch(Exception e){
+		e.printStackTrace();
+	}
+	}
+	
+	
+	@Test
+	public void sendSMS2() throws Exception{
+		try{
+			ParameterEntry  parameterEntry = new ParameterEntry();
+			List<String> list = new ArrayList();
+			list.add("18560042");
+			parameterEntry.setCarnum("carnum");
+			parameterEntry.setDate("date");
+			parameterEntry.setParameter1("parameter1");
+			parameterEntry.setParameter2("parameter2");
+			parameterEntry.setParameter3("parameter3");
+			parameterEntry.setParameter4("parameter4");
+			parameterEntry.setSms("尊敬的12车主，您的车辆保险将于12到期。【天成车业】");
+			parameterEntry.setSmsPwd("A3934081D6F869C3295618159571");
+			parameterEntry.setSmsUser("GL_tiancheng");
+			parameterEntry.setStoreName("storeName");
+			System.out.println("+++=======短信实体类"+parameterEntry);
+		    String sendSMS2 = sendSmsService.sendSMS2("0533001", "测试", list, parameterEntry);
+		    System.out.println(sendSMS2);
+		}catch(Exception e){
 		e.printStackTrace();
 	}
 	}
