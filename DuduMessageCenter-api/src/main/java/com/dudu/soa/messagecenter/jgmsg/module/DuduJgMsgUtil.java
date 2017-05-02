6584211ada4784473bf620ea8e1c2860a9e5af8a
miaohao <8166405@qq.com>
2017-05-02 17:38:13 +0800

    新增默认title

--- DuduMessageCenter-api/src/main/java/com/dudu/soa/messagecenter/jgmsg/module/DuduJgMsgUtil.java
+++ DuduMessageCenter-api/src/main/java/com/dudu/soa/messagecenter/jgmsg/module/DuduJgMsgUtil.java
@@ -11,6 +11,14 @@ public final class DuduJgMsgUtil {
     }
 
     /**
+     * 获取默认默认的极光推送 通知title
+     * @return
+     */
+    public static String getDefaultNotificationTitle() {
+        return "嘟嘟店管家";
+    }
+
+    /**
      * 获取极光推送消息模板初始化配置   基于别名
      *
      * @param loadId     登录Id（标签）
@@ -20,6 +28,7 @@ public final class DuduJgMsgUtil {
     public static JgMsgTemplate getBaseJgMsgTemplateByAlias(String loadId, String targetPath) {
         return new JgMsgTemplate()
                 .setAlias(loadId)
+                .setTitle(DuduJgMsgUtil.getDefaultNotificationTitle())
                 .putMap("path", targetPath);
     }
 
@@ -33,6 +42,7 @@ public final class DuduJgMsgUtil {
     public static JgMsgTemplate getBaseJgMsgTemplateByTags(List<String> tags, String targetPath) {
         return new JgMsgTemplate()
                 .addAllTags(tags)
+                .setTitle(DuduJgMsgUtil.getDefaultNotificationTitle())
                 .putMap("path", targetPath);
     }
 
