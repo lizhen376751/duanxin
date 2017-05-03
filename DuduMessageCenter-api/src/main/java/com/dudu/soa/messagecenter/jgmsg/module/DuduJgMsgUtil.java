b1eb960bd966df009dca1ae4dd5e0a5cba40e010
miaohao <8166405@qq.com>
2017-05-03 13:58:41 +0800

    路径分隔符修改

--- DuduMessageCenter-api/src/main/java/com/dudu/soa/messagecenter/jgmsg/module/DuduJgMsgUtil.java
+++ DuduMessageCenter-api/src/main/java/com/dudu/soa/messagecenter/jgmsg/module/DuduJgMsgUtil.java
@@ -22,28 +22,51 @@ public final class DuduJgMsgUtil {
      * 获取极光推送消息模板初始化配置   基于别名
      *
      * @param loadId     登录Id（标签）
-     * @param targetPath 目标标识
+     * @param targetPaths 目标标识
      * @return 消息模板
      */
-    public static JgMsgTemplate getBaseJgMsgTemplateByAlias(String loadId, String targetPath) {
+    public static JgMsgTemplate getBaseJgMsgTemplateByAlias(String loadId, String... targetPaths) {
         return new JgMsgTemplate()
                 .setAlias(loadId)
                 .setTitle(DuduJgMsgUtil.getDefaultNotificationTitle())
-                .putMap("path", targetPath);
+                .putMap("path", getTargePath(targetPaths));
     }
 
     /**
      * 获取极光推送消息模板 初始化配置 基于 标签
      *
      * @param tags       标签
-     * @param targetPath 目标标识
+     * @param targetPaths 目标标识
      * @return 消息模板
      */
-    public static JgMsgTemplate getBaseJgMsgTemplateByTags(List<String> tags, String targetPath) {
+    public static JgMsgTemplate getBaseJgMsgTemplateByTags(List<String> tags, String... targetPaths) {
         return new JgMsgTemplate()
                 .addAllTags(tags)
                 .setTitle(DuduJgMsgUtil.getDefaultNotificationTitle())
-                .putMap("path", targetPath);
+                .putMap("path", getTargePath(targetPaths));
+    }
+
+    /**
+     * 路径分隔符
+     */
+    private static final String SEPARATOR = ".";
+
+    /**
+     * 返回节点路径解析规则
+     *
+     * @param targetPaths  动态路径参数
+     * @return 路径分隔符
+     */
+    private static String getTargePath(String... targetPaths) {
+        StringBuilder key = new StringBuilder();
+        for (String path : targetPaths) {
+            if (key.length() == 0) {
+                key.append(path);
+            } else {
+                key.append(SEPARATOR).append(path);
+            }
+        }
+         return key.toString();
     }
 
 }
